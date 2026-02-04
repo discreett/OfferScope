34d8eb2d040ef464a467c4ce4a4e51071b451037
vd <vdinh2999@gmail.com>
2026-02-04 17:43:17 -0600

    Dropzone

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
-
+'use client'
+import Dropzone from "@/components/Dropzone";
 export default function Home() {
   return (
     <main>
       <div>
+        <h1>OfferScope</h1>
+        <p>Prepare your future in real time.</p>
 
+        <Dropzone />
       </div>
     </main>
   );
