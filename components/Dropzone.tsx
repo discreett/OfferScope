cdeea81e82d5ff842d63c286f7ff6cb45f744da1
vd <vdinh2999@gmail.com>
2026-02-04 18:20:50 -0600

    Image preview

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -1,23 +1,47 @@
 'use client'
-import React, {useCallback} from 'react'
+import React, {useCallback, useState} from 'react'
 import {useDropzone} from 'react-dropzone'
+import styles from '@/styles/dropzone.module.css'
 
 function MyDropzone() {
-  const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles);
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    const [files, setFiles] = useState([]);
+    const onDrop = useCallback(acceptedFiles => {
+        if (acceptedFiles?.length) {
+            setFiles(previousFiles => [
+                ...previousFiles, 
+                ...acceptedFiles.map(file =>
+                    Object.assign(file, {preview: URL.createObjectURL(file)})
+                )
+            ]);
+        }
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
-  )
-}
+    return (
+        <form>
+            <div {...getRootProps(
+                {
+                    className: styles.container
+                }
+            )}>
+        <input {...getInputProps()} />
+        {
+            isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+        </div>
+
+        <ul>
+            {files.map(file => (
+                <li key={file.name}>
+                    <img src={file.preview} alt={file.name} width={100} height={100} />
+                    <p>{file.name}</p>
+                </li>
+            ))}
+        </ul>
+        </form>
+    )
+    }
 
 export default MyDropzone;
