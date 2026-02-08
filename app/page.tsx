117cbb9c24a8342461c7e586a1b7780638e51a8f
vd <vdinh2999@gmail.com>
2026-02-07 22:41:29 -0600

    basic front page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,28 @@ export default function Home() {
   return (
     <main>
       <div>
-        <h1>OfferScope</h1>
+        <header>
+          <h1>OfferScope</h1>
+          <nav>Placeholder</nav>
+        </header>
+        
         <p>Prepare your future in real time.</p>
+        <p>Upload offer letters. Parse information in real time. Powered by A.I.</p>
         <Dropzone />
+
+        <footer>
+          <p>This tool provides informational summaries and comparisons only.</p>
+          <ul>
+              <li>It does not provide:</li>
+
+              <li>Legal advice</li>
+
+              <li>Financial advice</li>
+
+              <li>Employment advice</li>
+          </ul>
+        </footer>
+
       </div>
     </main>
   );
