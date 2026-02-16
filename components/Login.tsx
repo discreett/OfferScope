4adcfd5662bbdd6d98aae80c3b988cbe4382a940
vd <vdinh2999@gmail.com>
2026-02-16 13:19:46 -0600

    login component

--- components/Login.tsx
+++ components/Login.tsx
@@ -6,6 +6,23 @@ function myLogin() {
     return (
         <main className="min-h-screen">
             <Navbar/>
+            <section className="flex items-center justify-center py-25">
+                <div className="bg-white p-10 rounded-lg shadow-md shadow-[#7B68EE] w-full max-w-md">
+                    <h2 className="text-2xl font-bold mb-6 text-gray-800">Log In</h2>
+                    <h3 className="text-1xl font-bold mb-6 text-gray-800">Don't have an account? <a href="/signup" className="text-[#B8B8FF]">Sign up</a></h3>
+                    <form className="space-y-4">
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+                            <input type="email" id="email" name="email" className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-[#7B68EE] focus:border-[#7B68EE]" placeholder="you@example.com" />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                            <input type="password" id="password" name="password" className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-[#7B68EE] focus:border-[#7B68EE]" placeholder="Your Password" />
+                        </div>
+                        <button type="submit" className="w-full bg-[#7B68EE] text-white py-2 px-4 rounded-md shadow-sm hover:bg-[#B6B8D6] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#7B68EE]">Sign In</button>
+                    </form>
+                </div>
+            </section>
             <Footer/>
         </main>
     )
