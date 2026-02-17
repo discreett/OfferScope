ead23664a31cb14750ad51c086efbba323113f22
vd <vdinh2999@gmail.com>
2026-02-17 12:30:46 -0600

    signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import Signup from "@/components/Signup";
+
+export default function mySignup() {
+    return (
+        <main>
+            <Signup/>
+        </main> 
+    )
+}
---
+++ components/Signup.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function mySignup() {
+    return (
+        <main className="min-h-screen">
+            <Navbar/>
+            <Footer/>
+        </main>
+    )
+}
+
+export default mySignup;
